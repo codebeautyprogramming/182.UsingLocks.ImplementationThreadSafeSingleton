a70e70e833e8bf9458dfee4a29329b468d4ae7ab
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-08-02 22:05:12 +0200

    Startup code for this chapter

---
+++ CookBook/Services/DesktopFileWatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Services
+{
+    public class DesktopFileWatcher
+    {
+        private static DesktopFileWatcher _instance;
+        public static DesktopFileWatcher Instance
+        {
+            get 
+            { 
+                if(_instance == null)
+                {
+                    _instance = new DesktopFileWatcher();
+                }
+                return _instance;
+            }
+        }
+        private DesktopFileWatcher() { }
+    }
+}
